b51f05cd73aa2be84dfa2b4816129b400941d08d
Victor Campos <campos.viictor@gmail.com>
2020-06-30 17:14:55 -0300

    removendo modal content

--- public/scripts/foodfy.js
+++ public/scripts/foodfy.js
@@ -1,23 +1,10 @@
-const modalOverlay = document.querySelector ('.modal-overlay')
 const cards = document.querySelectorAll ('.card')
 
 
 for ( let card of cards ) {
   card.addEventListener ('click', () => {
-    modalOverlay.classList.add ('active')
     const imageId = card.getAttribute ('id')
     const pratoId = card.getAttribute ('name') 
     const chefId = card.getAttribute ('chef')
-    modalOverlay.querySelector ('img').src = `/${imageId}`
-    modalOverlay.querySelector ('h2').textContent = `${pratoId}`
-    modalOverlay.querySelector ('p').textContent = `${chefId}`
   })
 }
-
-/* MANIPULANDO CLOSE-MODAL */
-document.querySelector ('.close-modal').addEventListener ('click', () => {
-  modalOverlay.classList.remove ('active')
-  modalOverlay.querySelector ('img').src = ''
-  modalOverlay.querySelector ('h2').textContent = ''
-  modalOverlay.querySelector ('p').textContent = ''
-})
