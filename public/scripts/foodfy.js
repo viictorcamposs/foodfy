c6da996b62ea4cd37c580cac233e3d2cc736ddfe
Victor Campos <campos.viictor@gmail.com>
2020-07-01 13:50:06 -0300

    tornando dados das páginas dinâmicos

--- public/scripts/foodfy.js
+++ public/scripts/foodfy.js
@@ -3,8 +3,5 @@ const cards = document.querySelectorAll ('.card')
 
 for ( let card of cards ) {
   card.addEventListener ('click', () => {
-    const imageId = card.getAttribute ('id')
-    const pratoId = card.getAttribute ('name') 
-    const chefId = card.getAttribute ('chef')
   })
 }
--- server.js
+++ server.js
@@ -17,17 +17,37 @@ nunjucks.configure ('views', {
 
 // CONFIGURANDO CAMINHO DA HOME.NJK
 server.get ('/', function (req, res) {
-  return res.render ('home')
+  return res.render ('home', { data })
 })
 
 // CONFIGURANDO CAMINHO ABOUT.NJK
 server.get ('/about', function (req, res) {
-  return res.render ('about')
+  const dataPage = [
+    {
+      class: "sobre",
+      title: "Sobre o Foodfy",
+      paragraph: "Suspendisse placerat neque neque. Morbi dictum nulla non sapien rhoncus, et mattis erat commodo. Aliquam vel lacus a justo mollis luctus. Proin vel auctor eros, sed eleifend nunc. Curabitur eget tincidunt risus. Mauris malesuada facilisis magna, vitae volutpat sapien tristique eu. Morbi metus nunc, interdum in erat placerat, aliquam iaculis massa. Duis vulputate varius justo pharetra maximus. In vehicula enim nec nibh porta tincidunt. Vestibulum at ultrices turpis, non dictum metus. Vivamus ligula ex, semper vitae eros ut, euismod convallis augue.",
+      paragraph2: "Fusce nec pulvinar nunc. Duis porttitor tincidunt accumsan. Quisque pulvinar mollis ipsum ut accumsan. Proin ligula lectus, rutrum vel nisl quis, efficitur porttitor nisl. Morbi ut accumsan felis, eu ultrices lacus. Integer in tincidunt arcu, et posuere ligula. Morbi cursus facilisis feugiat. Praesent euismod nec nisl at accumsan. Donec libero neque, vulputate semper orci et, malesuada sodales eros. Nunc ut nulla faucibus enim ultricies euismod."
+    },
+    {
+      class: "historia",
+      title: "Como tudo começou...",
+      paragraph: "Suspendisse placerat neque neque. Morbi dictum nulla non sapien rhoncus, et mattis erat commodo. Aliquam vel lacus a justo mollis luctus. Proin vel auctor eros, sed eleifend nunc. Curabitur eget tincidunt risus. Mauris malesuada facilisis magna, vitae volutpat sapien tristique eu. Morbi metus nunc, interdum in erat placerat, aliquam iaculis massa. Duis vulputate varius justo pharetra maximus. In vehicula enim nec nibh porta tincidunt. Vestibulum at ultrices turpis, non dictum metus. Vivamus ligula ex, semper vitae eros ut, euismod convallis augue.",
+      paragraph2: "Fusce nec pulvinar nunc. Duis porttitor tincidunt accumsan. Quisque pulvinar mollis ipsum ut accumsan. Proin ligula lectus, rutrum vel nisl quis, efficitur porttitor nisl. Morbi ut accumsan felis, eu ultrices lacus. Integer in tincidunt arcu, et posuere ligula. Morbi cursus facilisis feugiat. Praesent euismod nec nisl at accumsan. Donec libero neque, vulputate semper orci et, malesuada sodales eros. Nunc ut nulla faucibus enim ultricies euismod."
+    },
+    {
+      class: "receitas",
+      title: "Nossas receitas",
+      paragraph: "Fusce nec pulvinar nunc. Duis porttitor tincidunt accumsan. Quisque pulvinar mollis ipsum ut accumsan. Proin ligula lectus, rutrum vel nisl quis, efficitur porttitor nisl. Morbi ut accumsan felis, eu ultrices lacus. Integer in tincidunt arcu, et posuere ligula. Morbi cursus facilisis feugiat. Praesent euismod nec nisl at accumsan. Donec libero neque, vulputate semper orci et, malesuada sodales eros. Nunc ut nulla faucibus enim ultricies euismod.",
+      paragraph2: ""
+    }
+  ]
+  return res.render ('about', { items: dataPage })
 })
 
 // CONFIGURANDO CAMINHO RECIPES.NJK
 server.get ('/recipes', function (req, res) {
-  return res.render ('recipes')
+  return res.render ('recipes', { data })
 })
 
 
