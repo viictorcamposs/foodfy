dc93a8114222af414978be380828b1c3629ab3b0
Victor Campos <campos.viictor@gmail.com>
2020-09-02 10:53:34 -0300

    criando gerenciador de imagens no front-end

--- public/scripts/admin.js
+++ public/scripts/admin.js
@@ -29,16 +29,91 @@
 
 
 // LÓGICA PARA FAZER UPLOAD DE FOTOS DAS RECEITAS
-    const photosUpload = {
+    const PhotosUpload = {
+        input: "",
         uploadLimit: 5,
+        preview: document.querySelector( '#photos-preview' ),
+        files: [],
         handleFileInput( event ) {
             const { files: fileList } = event.target 
-            const { uploadLimit } = photosUpload
+            PhotosUpload.input = event.target
+            
+            if ( PhotosUpload.hasLimit( event ) ) return
 
-            if ( fileList.length > uploadLimit ) {
+            Array.from( fileList ).forEach( file => {   
+                PhotosUpload.files.push( file )
+
+                const reader = new FileReader()
+                reader.onload = () => {
+                    const image = new Image()
+                    image.src = String( reader.result )
+
+                    const container = PhotosUpload.getContainer( image )
+                    PhotosUpload.preview.appendChild( container )
+                }
+                reader.readAsDataURL( file )
+            })
+
+            PhotosUpload.input.files = PhotosUpload.getAllFiles()
+        },
+        hasLimit( event ) {
+            const { uploadLimit, input, preview } = PhotosUpload
+            const { files: fileList } = input
+
+            if( fileList.length > uploadLimit ) {
                 alert( `Envie no máximo ${ uploadLimit } fotos.` )
                 event.preventDefault()
-                return 
+                return true
             }
+
+            const photosDiv = []
+            preview.childNodes.forEach( item => {
+                if( item.classList && item.classList.value == "photo" ) photosDiv.push( item )
+            })
+
+            const totalPhotos = fileList.length + photosDiv.length
+            if( totalPhotos > uploadLimit ) {
+                const rest = uploadLimit - photosDiv.length
+                if( rest > 1 ) {
+                    alert( `Coloque mais ${ rest } fotos.` )
+                } else if( rest == 1 ) {
+                    alert( `Coloque mais ${ rest } foto.` )
+                } else {
+                    alert( "Você atingiu o limite de fotos!" )
+                }
+                event.preventDefault()
+                return true
+            } 
+            return false
+        },
+        getAllFiles() {
+            const dataTransfer = new ClipboardEvent("").clipboardData || new DataTransfer()  
+            PhotosUpload.files.forEach( file => dataTransfer.items.add( file ) )
+
+            return dataTransfer.files
+        },
+        getContainer( image ) {
+            const container = document.createElement( 'div' )
+            container.classList.add( 'photo' )
+            container.onclick = PhotosUpload.removePhoto
+            container.appendChild( image )
+            container.appendChild( PhotosUpload.getRemoveButton() )
+            return container
+        },
+        getRemoveButton() {
+            const button = document.createElement( 'i' )
+            button.classList.add( 'material-icons' )
+            button.innerHTML = 'delete'
+            return button
+        },
+        removePhoto( event ) {
+            const photoContainer = event.target.parentNode // <div class="photo">
+            const photosArray = Array.from( PhotosUpload.preview.children )
+            const index = photosArray.indexOf( photoContainer )
+
+            PhotosUpload.files.splice( index, 1 )
+            PhotosUpload.input.files = PhotosUpload.getAllFiles()
+
+            photoContainer.remove()
         }
     }
